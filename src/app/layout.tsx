a3094566ab32faa7645d25e5ee5bbcf80e7b1e43
Utkarsh Singh <145763543+UtkarshSingh-1@users.noreply.github.com>
2026-01-08 15:49:17 +0530

    Add Razorpay checkout script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,8 @@ export default async function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        {/* Razorpay Script */}
+        <script src="https://checkout.razorpay.com/v1/checkout.js" />
       </head>
       <body className={inter.className}>
         <SessionProvider session={session}>
